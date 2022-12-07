c74a62d99b857da56388c79f7b2ebada92073ebc
Oscar Rojas <oscarrojasb.tec@gmail.com>
2022-12-06 23:47:21 -0500

    Definición de los métodos o vistas para el home, mostrar un post y crear-modificar posts. Adicional se incorpora los template para renderizar el contenido HTML

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
+posts = []
+
 @app.route("/")
-def hello_word():
-    return "Hello, World"
+def index():
+    return render_template("index.html", num_posts= len(posts))
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("show_post.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
